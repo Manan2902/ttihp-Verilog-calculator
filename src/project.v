f581e84c9804d998bf2100aa5cea16f533e4249f
Manan <97438921+Manan2902@users.noreply.github.com>
2025-07-19 19:09:57 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_calculator (
+module tt_um_manan_calculator (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,7 +17,17 @@ module tt_um_calculator (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    wire [7:0] io_in  = ui_in;
+    wire [7:0] io_out;
+
+    calculator calc (
+        .io_in(io_in),
+        .io_out(io_out)
+    );
+    
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = io_out;
+  
   assign uio_out = 0;
   assign uio_oe  = 0;
 
