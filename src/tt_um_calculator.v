aa0b5538d3d0b7006ccde37bb5cd1c8281f3d0f7
Manan <97438921+Manan2902@users.noreply.github.com>
2025-07-26 01:08:41 +0530

    Update tt_um_calculator.v

--- src/tt_um_calculator.v
+++ src/tt_um_calculator.v
@@ -34,6 +34,6 @@ module tt_um_calculator (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{uio_in, ena, clk, rst_n, 1'b0};
 
 endmodule
